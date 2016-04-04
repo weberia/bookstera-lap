e9e71dd0cabe1fbc457bb9a2559c6c5f7aef349c
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2016-04-04 17:17:39 +0700

    ---

--- bookstera.js
+++ bookstera.js
@@ -1,24 +1,117 @@
 'use strict';
 
 const Hapi = require('hapi');
+var Path = require('path');
+var Inert = require('inert');
+
+const server = new Hapi.Server();
+server.connection({ port: 8000 });
+
+var r = require('rethinkdb')
+var cn = null;
+r.connect( {host: 'archera', port: 28015, db: 'bookstera'}, function(err, conn) {
+  if (err) throw err;
+  cn = conn;
+})
+
+server.register(require('vision'), (err) => {
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: 'views/common'
+  });
+});
+
+server.register(Inert, (err) => {
+
+  if (err) {
+    throw err;
+  }
+
+  server.route({
+
+    method: 'GET',
+    path: '/assets/{param*}',
+    handler: {
+      directory: {
+        path: Path.join(__dirname, 'public'),
+        listing: true
+      }
+    }
+
+  });
+
+});
 
 let uuid = 1;       // Use seq instead of proper unique identifiers for demo only
 
 const users = {
-    john: {
-        id: 'john',
-        password: 'password',
-        name: 'John Doe'
-    }
+  buyer: {
+    id: 'buyer',
+    password: '123',
+    name: 'XYZ University'
+  },
+  seller: {
+    id: 'seller',
+    password: '123',
+    name: 'Bookstera Expert'
+  }
 };
 
 const home = function (request, reply) {
 
-    reply('<html><head><title>Login page</title></head><body><h3>Welcome ' +
-      request.auth.credentials.name +
-      '!</h3><br/><form method="get" action="/logout">' +
-      '<input type="submit" value="Logout">' +
-      '</form></body></html>');
+    reply.view('index', { 
+      message: 'Welcome ' + 
+               request.auth.credentials.name + 
+              '!</h3><br/><form method="get" action="/logout">' +
+              '<input type="submit" value="Logout">' +
+              '</form></body>',
+      whoami: request.auth.credentials.name,
+      title: 'Home'
+    });
+
+};
+
+const goals = function (request, reply) {
+
+    reply.view('index', { 
+      message: 'Msg goals',
+      whoami: request.auth.credentials.name,
+      title: 'Goals and Progress'
+    });
+
+};
+
+const ontology = function (request, reply) {
+
+    reply.view('index', { 
+      message: 'Msg ontology',
+      whoami: request.auth.credentials.name,
+      title: 'Ontology and Shared Meaning'
+    });
+
+};
+
+const notes = function (request, reply) {
+
+    reply.view('index', { 
+      message: 'Msg notes',
+      whoami: request.auth.credentials.name,
+      title: 'Notes'
+    });
+
+};
+
+const discourses = function (request, reply) {
+
+    reply.view('index', { 
+      message: 'Msg discourses',
+      whoami: request.auth.credentials.name,
+      title: 'Discourses'
+    });
+
 };
 
 const login = function (request, reply) {
@@ -27,7 +120,7 @@ const login = function (request, reply) {
         return reply.redirect('/');
     }
 
-    let message = '';
+    let mmessage = '';
     let account = null;
 
     if (request.method === 'post') {
@@ -35,27 +128,27 @@ const login = function (request, reply) {
         if (!request.payload.username ||
             !request.payload.password) {
 
-            message = 'Missing username or password';
+            mmessage = 'Missing username or password';
         }
         else {
             account = users[request.payload.username];
             if (!account ||
                 account.password !== request.payload.password) {
 
-                message = 'Invalid username or password';
+                mmessage = 'Invalid username or password';
             }
         }
     }
 
     if (request.method === 'get' ||
-        message) {
+        mmessage) {
 
-        return reply('<html><head><title>Login page</title></head><body>' +
-            (message ? '<h3>' + message + '</h3><br/>' : '') +
+        return reply.view('login', { message: 
+            (mmessage ? '<h3>' + mmessage + '</h3><br/>' : '') +
             '<form method="post" action="/login">' +
             'Username: <input type="text" name="username"><br>' +
             'Password: <input type="password" name="password"><br/>' +
-            '<input type="submit" value="Login"></form></body></html>');
+            '<input type="submit" value="Login"></form>'});
     }
 
     const sid = String(++uuid);
@@ -76,8 +169,6 @@ const logout = function (request, reply) {
     return reply.redirect('/');
 };
 
-const server = new Hapi.Server();
-server.connection({ port: 8000 });
 
 server.register(require('hapi-auth-cookie'), (err) => {
 
@@ -111,13 +202,65 @@ server.register(require('hapi-auth-cookie'), (err) => {
     });
 
     server.route([
-        { method: 'GET', path: '/', config: { handler: home } },
-        { method: ['GET', 'POST'], path: '/login', config: { handler: login, auth: { mode: 'try' }, plugins: { 'hapi-auth-cookie': { redirectTo: false } } } },
-        { method: 'GET', path: '/logout', config: { handler: logout } }
+        { 
+          method: 'GET', 
+          path: '/', 
+          config: { 
+            handler: home 
+          } 
+        },
+        { 
+          method: ['GET', 'POST'], 
+          path: '/login', 
+          config: { 
+            handler: login, auth: 
+              { mode: 'try' }, 
+              plugins: { 
+                'hapi-auth-cookie': 
+                  { 
+                    redirectTo: false 
+                  } 
+              } 
+          } 
+        },
+        { 
+          method: 'GET', 
+          path: '/logout', 
+          config: { 
+            handler: logout 
+          } 
+        },
+        {
+          method: 'GET',
+          path: '/goals',
+          config: {
+            handler: goals
+          }
+        },
+        {
+          method: 'GET',
+          path: '/ontology',
+          config: {
+            handler: ontology
+          }
+        },
+        {
+          method: 'GET',
+          path: '/notes',
+          config: {
+            handler: notes
+          }
+        },
+        {
+          method: 'GET',
+          path: '/discourses',
+          config: {
+            handler: discourses
+          }
+        },
     ]);
 
     server.start(() => {
-
         console.log('Server ready');
     });
 });
